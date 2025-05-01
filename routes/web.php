a4ddfcdd84045213278c38d52f20964fb74d3047
Althaf Rizal <ilikemud7@gmail.com>
2025-05-01 16:24:02 +0700

    fix routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenukaranController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,11 +12,7 @@ Route::get('/home', [HomeController::class, 'index']);
 
 //gunakan controller untuk memanggil view penukaran
 Route::get('/penukarans1', [PenukaranController::class, 'penukaran1']);
-// Route::get('/penukarans2', [PenukaranController::class, 'penukaran2']);
+Route::get('/penukarans2', [PenukaranController::class, 'penukaran2']);
 Route::get('/penukarans3', [PenukaranController::class, 'penukaran3']);
 Route::get('/penukarans3_2', [PenukaranController::class, 'penukaran3_2']);
 
-//return view penukarans2
-Route::get('/penukarans2', function () {
-    return view('penukarans2');
-});
