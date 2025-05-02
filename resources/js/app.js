2c97bfe5afbf50d4afb86503035368deb67b3c30
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-02 09:37:53 +0700

    added new component

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ import PenukaranStep3_2 from './pages/PenukaranStep3_2.vue';
 import PenukaranKoin from './pages/PenukaranKoin.vue';
 import Jelajah from './pages/Jelajah.vue';
 import PenukaranStep1 from './pages/PenukaranStep1.vue';
+import Beranda from './pages/Beranda.vue';
+import PenukaranStep3 from './pages/PenukaranStep3.vue';
 
 const el = document.getElementById('app');
 
@@ -14,7 +16,9 @@ if (el) {
   let component = null;
 
   switch (page) {
-    // case 'beranda':
+    case 'beranda':
+        component = Beranda;
+        break;
 
     case 'penukaran-step-1':
       component = PenukaranStep1;
@@ -24,9 +28,9 @@ if (el) {
     case 'penukaran-step-2':
       component = PenukaranStep2;
       break;
-    // case 'penukaran-step-3':
-
-    //   break;
+    case 'penukaran-step-3':
+        component = PenukaranStep3;
+        break;
     case 'penukaran-step-3-2':
         component = PenukaranStep3_2;
         break;
