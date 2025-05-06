935065e1ced9247c2f0f9e162cb6f30215a8a44f
DelfanDZW <delfanzuffarrajjaznuziar@gmail.com>
2025-05-06 12:56:00 +0800

    berandaAdd

---
+++ resources/js/components/FeatureItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex w-full max-w-xs items-start space-x-4">
+        <div class="flex-shrink-0">
+            <img :src="icon" class="text-teal-600" />
+        </div>
+        <div>
+            <h3 class="text-xl font-semibold text-gray-800">{{ title }}</h3>
+            <p class="text-sm text-gray-600">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    icon: String,
+    title: String,
+    description: String,
+});
+</script>
