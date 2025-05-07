de6387e1bfc065add2bc5a1710e0a233ca10883d
Althaf Rizal <ilikemud7@gmail.com>
2025-05-07 16:03:57 +0700

    update user database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('saldo_koin')->default(0);
+            $table->string('akun_facebook')->nullable();
+            $table->string('akun_twitter')->nullable();
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
 
