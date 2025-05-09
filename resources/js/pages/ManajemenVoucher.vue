670e26e961d139df85c83e029b1e75cc4d1392a7
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-09 13:47:29 +0700

    tambah Manajemen Voucher (belum selesai)

---
+++ resources/js/pages/ManajemenVoucher.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="manajemen-voucher">
+
+    </div>
+</template>
+
+<style scoped></style>
---
+++ resources/views/manajemen_voucher.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+
+@section('title', 'Manajemen Voucher')
+@section('page', 'manajemen-voucher')
+
+@section('content')
+    <div id="app" data-page="manajemen-voucher"></div>
+@endsection
