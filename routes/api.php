340623b177b61e10b23dd23eb44e182554945569
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-26 08:49:28 +0700

    tambah fungsi ambil data user dan voucher

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PenukaranKoinController;
+use App\Http\Controllers\AdminPageController;
 
 Route::middleware(['web'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
@@ -16,3 +17,7 @@ Route::post('/register', [AuthController::class, 'register'])->name('auth.regist
 Route::middleware(['web', 'auth'])->group(function () { // Gunakan middleware 'web'
     Route::get('/get-user', [AuthController::class, 'getUser'])->name('auth.getUser');
 });
+
+// Fetch data
+Route::get('/vouchers', [AdminPageController::class, 'getAllVouchers']);
+Route::get('/users', [AdminPageController::class, 'getAllUsers']);
