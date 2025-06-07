4a07b452fb5257e22b64cef1cde9581b33e3ae90
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-06-07 22:43:13 +0700

    tambah jetstream

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,13 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('saldo_koin')->default(0);
-            $table->string('akun_facebook')->nullable();
-            $table->string('akun_twitter')->nullable();
-            $table->enum('role', ['admin', 'user'])->default('user');
-            $table->enum('status', ['active', 'banned'])->default('active');
-            $table->string('photo_profile_url')->nullable();
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
 
