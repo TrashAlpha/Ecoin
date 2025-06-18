1452d649625884432035119fbd3f3c0a12e468da
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-06-18 21:54:33 +0700

    tambah url web vercel

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,19 +15,19 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $allowedOrigins = ['http://127.0.0.1:8000', 'http://localhost:8000'];
+        $allowedOrigins = ['http://127.0.0.1:8000', 'http://localhost:8000', 'https://ecoin-hosted.vercel.app'];
         $origin = $request->header('Origin');
-        
+
         if (in_array($origin, $allowedOrigins)) {
             $response = $next($request)
                 ->header('Access-Control-Allow-Origin', $origin)
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
                 ->header('Access-Control-Allow-Credentials', 'true');
-            
+
             return $response;
         }
-        
+
         return $next($request);
     }
 }
